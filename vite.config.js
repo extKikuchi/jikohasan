79f20a4ae5afa58c3cd7614cff0557964b854a20
extKikuchi <kikuchi@ext.cx>
2025-07-11 13:40:33 +0900

    add

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/my-ocr-prototype/',  // ここをリポジトリ名に合わせる
+  base: '/jikohasan/',  // ここをリポジトリ名に合わせる
   plugins: [react()],
   build: {
     outDir: 'docs'
